f0633ad8411d56950820c0f2d5a3a724bff49b7a
codelover <codelovercc@gmail.com>
2024-12-26 00:05:02 +0800

    Modify dart workflow, adding tests

--- test/dart_logging_abstraction_test.dart
+++ test/dart_logging_abstraction_test.dart
@@ -1,1 +1,25 @@
-void main() {}
+import 'package:dart_logging_abstraction/dart_logging_abstraction.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  test("Example logging", () {
+    // Logging implementation example can find in lib/src/console_logging.dart
+    final traceLogger = LoggerFactory(minLevel: LogLevel.trace).createLogger<MyClass>();
+    traceLogger.fatal("Fatal on traceLogger", error: Error(), stackTrace: StackTrace.current);
+    traceLogger.error("Error on traceLogger", error: Error(), stackTrace: StackTrace.current);
+    traceLogger.warning("Warning on traceLogger");
+    traceLogger.info("Info on traceLogger");
+    traceLogger.debug("Debug on traceLogger");
+    traceLogger.trace("trace on traceLogger");
+
+    final infoLogger = LoggerFactory(minLevel: LogLevel.info).create("InfoLogger");
+    infoLogger.fatal("Fatal on infoLogger", error: Error(), stackTrace: StackTrace.current);
+    infoLogger.error("Error on infoLogger", error: Error(), stackTrace: StackTrace.current);
+    infoLogger.warning("Warning on infoLogger");
+    infoLogger.info("Info on infoLogger");
+    infoLogger.debug("Debug on infoLogger");
+    infoLogger.trace("trace on infoLogger");
+  });
+}
+
+class MyClass {}
